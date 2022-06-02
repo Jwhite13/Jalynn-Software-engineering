aab4d6d7c232ecd17d84b8faddc11ca45e3d5565
Jwhite13 <106295247+Jwhite13@users.noreply.github.com>
2022-06-02 10:12:39 -0500

    Add introductory site files

---
+++ PRE-WORK/action_page.php
@@ -0,0 +1,27 @@
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+  <title>Jalynn's Website</title>
+  <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+
+<body>
+
+<!-- Site navigation menu -->
+<ul class="navbar">
+  <li><a href="index.html">Home page</a>
+  <li><a href="pictures.html">Pictures</a>
+  <li><a href="videos.html">Videos</a>
+  <li><a href="forms.html">Forms</a>
+</ul>
+
+<!-- Main content -->
+Your First name is <?php echo $_GET["fname"]; ?><br>
+Your Last name is <?php echo $_GET["fname"]; ?>
+
+
+</body>
+</html>
